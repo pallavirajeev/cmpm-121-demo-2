4d8f8e3bb996b61d049afb5a6d383b631344baae
Pallavi Rajeev <pallavirajeev44@gmail.com>
2023-10-17 18:55:28 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Pallavi's game";
 
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.style.border = "1px solid black"; //thin black border
+canvas.style.borderRadius = "10px"; //rounded corners
+canvas.style.boxShadow = "2px 2px 10px rgba(0, 0, 0, 0.3)"; //drop shadow
+
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+app.appendChild(canvas);
